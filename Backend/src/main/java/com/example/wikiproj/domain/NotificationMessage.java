8ad9200651472d3fbe0bb9cdd7f39fdeb3d3b67a
ttt Uneugatoies <t3u33t333@gmail.com>
2023-12-04 20:49:54 +0900

    [BE] 사용자 알림 구현 1 - NotificationMessage entity 추가

---
+++ Backend/src/main/java/com/example/wikiproj/domain/NotificationMessage.java
@@ -0,0 +1,44 @@
+package com.example.wikiproj.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@Table(name = "notification_message")
+public class NotificationMessage extends BaseEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String message;
+	
+	@ManyToOne
+	@JoinColumn(name = "recipient")
+	private User recipient;
+	
+	@ManyToOne
+	@JoinColumn(name = "where")
+	private Wiki where;
+
+}
